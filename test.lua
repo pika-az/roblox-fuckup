67ccae47eb343a2392326e6042f944b3fbacc3e6
pika-az <124748859+pika-az@users.noreply.github.com>
2023-03-28 13:59:03 +0100

    Add files via upload

--- test.lua
+++ test.lua
@@ -1,12 +1,12 @@
+-- OrionLib Loadstring
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 OrionLib:MakeNotification({
 	Name = "Welcome pika!",
 	Content = "Islands GUI",
 	Image = "rbxassetid://6006991075",
 	Time = 5
 })
-
--- OrionLib Loadstring
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 -- Creating Gui
 local Window = OrionLib:MakeWindow({Name = "Islands GUI", HidePremium = false, SaveConfig = false, ConfigFolder = "Islands_pika"})
